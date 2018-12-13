9d05536381233bdd2741dbf755df220cae95debf
ashok_gawas <ashok_gawas@persistent.co.in>
2018-12-13 15:42:42 +0530

    committed as part of 123

--- src/main/java/com/psl/github/Intro.java
+++ src/main/java/com/psl/github/Intro.java
@@ -2,6 +2,8 @@ package com.psl.github;
 
 public class Intro {
     public static void main(String[] args) {
-        System.out.println("Paging github from IntelliJ");
+        System.out.println("This is github tutorial using IntelliJ IDE");
+        System.out.println("This is another sentence");
+        System.out.println("This is another sentence added to new branch");
     }
 }
