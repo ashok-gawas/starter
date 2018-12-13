a48538250fbbf7d368f522e59fe57239481b24e1
ashok_gawas <ashok_gawas@persistent.co.in>
2018-12-13 17:37:48 +0530

    changes after 1st stash

--- src/main/java/com/psl/github/Intro.java
+++ src/main/java/com/psl/github/Intro.java
@@ -4,5 +4,7 @@ public class Intro {
     public static void main(String[] args) {
         System.out.println("This is github tutorial using IntelliJ IDE");
         System.out.println("This is another sentence added to new branch");
+
+        System.out.println("This is a change after stashing");
     }
 }
